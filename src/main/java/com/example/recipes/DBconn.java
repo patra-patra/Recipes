1b6a3328d43ed8cb1061b66f98d72513eb20f85c
petrussich <alina.ocheretnaya@gmail.com>
2023-11-10 14:37:12 +1000

    prikol

---
+++ src/main/java/com/example/recipes/DBconn.java
@@ -0,0 +1,18 @@
+package com.example.recipes;
+import java.sql.*;
+public class DBconn {
+    private static String url = "jdbc:mysql://localhost:3306/recipes";
+    private static String user = "root";
+    private static String password = "root";
+
+    public static Connection GetConnection(){
+        Connection connection = null;
+
+        try{
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return connection;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.recipes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.recipes to javafx.fxml;
