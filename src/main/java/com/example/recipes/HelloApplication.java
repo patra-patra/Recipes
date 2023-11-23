26d500d0bac1d977eafb56c6a1ae2fdd1fbb5931
qwqqwq2016@yandex.ru <qwqqwq2016@yandex.ru>
2023-11-24 00:38:16 +1000

    Иконка с котёнком поварёнком

--- src/main/java/com/example/recipes/HelloApplication.java
+++ src/main/java/com/example/recipes/HelloApplication.java
@@ -2,43 +2,60 @@ package com.example.recipes;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 
-
 public class HelloApplication extends Application {
+
+
+    Button button;
     @Override
     public void start(Stage stage) throws IOException {
+
+        /*
+        stage.setTitle("Recipes");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main_page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 250);
+
+        stage.getScene();
+        stage.show();
+*/
+
+        stage.setTitle("Recipes");
+
+        Group root = new Group();
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root);
+
+        Image icon = new Image("kitty.jpg");
+        stage.getIcons().add(icon);
+
         stage.setScene(scene);
         stage.show();
-    }
-/*
-* function createRandomString(sumString){
-const symbolArr = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-var randomString = "";
-for (let i=0; i<sumString; i++){
-	var index = Math.floor(Math.random()*symbolArr.length);
-	randomString +=symbolArr[index];
-}
-return randomString;
-}
 
-console.log(createRandomString(10));
-*
-*
-* */
+
+    }
+
     public static void main( String[] args ) throws IOException{
+        launch(args);
+        /*
 
         String file = "111111"+".jpg";
         Pars pars = new Pars();
@@ -50,9 +67,6 @@ console.log(createRandomString(10));
         //pars.Ingridients(path);
         pars.Steps(path);
         //pars.Category(path);
-
-
-
         /*
         URL img = new URL ("https://saechka.ru/upload/iblock/884/001.jpg");
         InputStream is = img.openStream();
